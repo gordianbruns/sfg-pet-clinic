f047baba263f7fb0217d8056f38ad0b04b186a6f
gordianbruns <gordob1@web.de>
2021-07-05 06:46:40 -0400

    added specialities, added to vet

---
+++ pet-clinic-data/src/main/java/edu/depauw/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package edu.depauw.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
