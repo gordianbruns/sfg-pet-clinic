d992d91728c46c9d1d427d3d3f3ef93323d281ae
gordianbruns <gordob1@web.de>
2021-07-14 05:26:03 -0400

    added spring data jpa services for owners

--- pet-clinic-data/src/main/java/edu/depauw/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/edu/depauw/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,10 @@ package edu.depauw.sfgpetclinic.repositories;
 import edu.depauw.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
