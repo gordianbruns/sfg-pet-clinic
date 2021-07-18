0be734829018313f299d3f30b19e9e68d2b4ecd2
gordianbruns <gordob1@web.de>
2021-07-18 07:04:35 -0400

    added project lombok

--- pet-clinic-data/src/main/java/edu/depauw/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/edu/depauw/sfgpetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package edu.depauw.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "vets")
 public class Vet extends Person {
 
@@ -12,12 +19,4 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
